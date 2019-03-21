5cb5e04074b095c88a896c40d7f18cae9c080999
Chris Rodriguez <cerdevelopers@gmail.com>
2019-03-21 01:05:57 -0500

    UPDATE: • Random button now works and opens second activity with a random number

--- app/src/main/java/christopher/kotlinapp/MainActivity.kt
+++ app/src/main/java/christopher/kotlinapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package christopher.kotlinapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,5 +40,24 @@ class MainActivity : AppCompatActivity() {
 
         // Display the new value in the text view.
         showCountTextView.text = count.toString();
+    }
+
+    fun randomMe(view: View) {
+
+
+        // Create an Intent to start the second activity
+        val randomIntent = Intent(this, SecondActivity::class.java)
+
+        // Get the current value of the text view.
+        val countString = textView.text.toString()
+
+        // Convert the count to an int
+        val count = Integer.parseInt(countString)
+
+        // Add the count to the extras for the Intent.
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
+
+        // Start the new activity.
+        startActivity(randomIntent)
     }
 }
